875416f9b887f724a486db2517e3fbe0a4e948b2
Marc Bernini <bernini.marc@gmail.com>
2019-04-09 23:16:36 +0200

    Project Kitten Story created. Components of app created too, with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateKittenComponent } from './components/create-kitten/create-kitten.component';
+import { RacineComponent } from './pages/racine/racine.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'create', component: CreateKittenComponent
+  },
+  {
+    path: '', component: RacineComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RacineComponent } from './pages/racine/racine.component';
+import { CreateKittenComponent } from './components/create-kitten/create-kitten.component';
+import { ListKittenComponent } from './components/list-kitten/list-kitten.component';
+import { UserKittenComponent } from './components/user-kitten/user-kitten.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RacineComponent,
+    CreateKittenComponent,
+    ListKittenComponent,
+    UserKittenComponent
   ],
   imports: [
     BrowserModule,
